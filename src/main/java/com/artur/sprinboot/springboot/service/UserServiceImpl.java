a43801c1c0efb08237c6959ea972b527b4cf3a39
Artur Giniyatullin <archigini@gmail.com>
2025-02-19 19:08:03 +0500

    use @transactional, delete createRolesIfNotExist

--- src/main/java/com/artur/sprinboot/springboot/service/UserServiceImpl.java
+++ src/main/java/com/artur/sprinboot/springboot/service/UserServiceImpl.java
@@ -19,7 +19,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@Transactional
 public class UserServiceImpl implements UserService {
     private final RoleRepository roleRepository;
     private final UserRepository userRepository;
